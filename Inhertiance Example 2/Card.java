9f5dafbf5d45962534688a1505f348d428048f10
Adhisha <adhishagc@gmail.com>
2020-09-27 21:09:02 +0530

    Created classes and updated the card class

--- Inhertiance Example 2/Card.java
+++ Inhertiance Example 2/Card.java
@@ -8,75 +8,19 @@ public abstract class Card{
     protected String type;
 
 
-    public Card() {
-    }
+    protected abstract void setNumber();
+    protected abstract void setName();
+    protected abstract void setBank();
+    protected abstract void setAccountNumber();
+    protected abstract void setBalance();
+    protected abstract void setType();
 
-    public Card(int number, String name, String bank, int accountNumber, float balance, String type) {
-        this.number = number;
-        this.name = name;
-        this.bank = bank;
-        this.accountNumber = accountNumber;
-        this.balance = balance;
-        this.type = type;
-    }
-
-    public int getNumber() {
-        return this.number;
-    }
-
-    public void setNumber(int number) {
-        this.number = number;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getBank() {
-        return this.bank;
-    }
-
-    public void setBank(String bank) {
-        this.bank = bank;
-    }
-
-    public int getAccountNumber() {
-        return this.accountNumber;
-    }
-
-    public void setAccountNumber(int accountNumber) {
-        this.accountNumber = accountNumber;
-    }
-
-    public abstract float getBalance();
-
-    public void setBalance(float balance) {
-        this.balance = balance;
-    }
-
-    public String getType() {
-        return this.type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    @Override
-    public String toString() {
-        return "{" +
-            " number='" + getNumber() + "'" +
-            ", name='" + getName() + "'" +
-            ", bank='" + getBank() + "'" +
-            ", accountNumber='" + getAccountNumber() + "'" +
-            ", balance='" + getBalance() + "'" +
-            ", type='" + getType() + "'" +
-            "}";
-    }
+    protected abstract int getNumber();
+    protected abstract String getName();
+    protected abstract String getBank();
+    protected abstract int getAccountNumber();
+    protected abstract float getBalance();
+    protected abstract String getType();
 
     
 }
--- Inhertiance Example 2/Credit.java
+++ Inhertiance Example 2/Credit.java
@@ -1,3 +1,39 @@
 public class Credit extends Card{
+
+    private float usage;
+    private float percentage;
+
+
+    public Credit() {
+    }
+
+    public Credit(float usage, float percentage) {
+        this.usage = usage;
+        this.percentage = percentage;
+    }
+
+    public float getUsage() {
+        return this.usage;
+    }
+
+    public void setUsage(float usage) {
+        this.usage = usage;
+    }
+
+    public float getPercentage() {
+        return this.percentage;
+    }
+
+    public void setPercentage(float percentage) {
+        this.percentage = percentage;
+    }
+
+    public float remainder(){
+        float remainder = super.getBalance()*this.percentage - this.usage;
+    }
+    
+
     
 }
+
+    
